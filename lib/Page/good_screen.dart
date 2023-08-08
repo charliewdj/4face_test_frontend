f3a8f285219b553572a7e49b3be9b0ee05725cd3
Charlie <charlie.reamur@gmail.com>
2023-08-08 19:01:26 +0900

    charlie_0808_createproject7

--- lib/Page/good_screen.dart
+++ lib/Page/good_screen.dart
@@ -1,5 +1,6 @@
 
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
@@ -16,47 +17,80 @@ class GoodScreen extends StatelessWidget {
           title: Row(
             children: [
               Padding(
-                padding: const EdgeInsets.fromLTRB(0, 12, 174, 12),
+                padding: const EdgeInsets.fromLTRB(12, 12, 174, 12),
                 child: Icon(Icons.arrow_back_ios, color: Colors.black,),
               ),
 
-              Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 12, 80, 12),
-                    child: Text('Good', style: TextStyle(
+              Align(
+                alignment: Alignment.center,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(180, 12, 80, 12),
+                  child: Text('Good', style: TextStyle(
                       color: Colors.black
-                    ),),
-                  ),
-
-              SizedBox(height: 32,),
-              MasonryGridView.count(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics
-                ( ),
-                crossAxisCount: 2,
-                itemCount: 5,
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 20
+                  ),),
                 ),
-                itemBuilder: (context, index){
-                  return Column(
-                    children: [
-                      Stack(
-                        children: [
-                          Positioned(
-                            child: ClipRect(
-
-                            ),
-                      )
-                    ],
-                  )
-                    ],
-                  );
-                },
               ),
+
+              SizedBox(height: 1,),
+              Grid(),
             ],
           )
       ),
     );
+
   }
 
+}
+
+class Grid extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      gridDelegate:const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 12.0,
+          mainAxisSpacing: 12.0,
+          mainAxisExtent: 300,
+      ),
+      itemCount: 5,
+      itemBuilder: (_,index){
+        return Container(
+          height: 250,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(
+              16.0
+          ),
+          color: Color(0xf00000099),
+          ),
+          child: Column(
+            children: [
+              ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(16.0),
+                    topRight: Radius.circular(16.0),
+                  ),
+                  child: Image.network(
+                      'https://shorturl.at/fgwH1',
+                    height: 170,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  )
+              ),
+              Padding(padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Minami'),
+                    const SizedBox(height: 8.0,),
+                    Text('25歳・東京'),
+              ],),)
+            ],
+          ),
+        );
+      },
+    );
+  }
 }
--- lib/Page/match_screen.dart
+++ lib/Page/match_screen.dart
@@ -196,6 +196,8 @@ class ChatBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
       children: [
         Padding(
           padding: EdgeInsets.fromLTRB(16, 16, 16, 8),
@@ -208,10 +210,170 @@ class ChatBox extends StatelessWidget {
             ),
           ),
         ),
-        //activechats
+        //ActiveChats(),
+        RecentChat(),
       ],);
   }
 }
+
+class ActiveChats extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.only(top: 25, left: 5),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              for (int i = 0; i < 10; i++)
+              Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                  child: Container(
+                    width: 65,
+                    height: 65,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(35),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          blurRadius: 10,
+                          spreadRadius: 2,
+                          offset: Offset(0,3),
+                        )
+                      ]
+                    ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(35),
+                        child: Image.asset(
+                            'images/profile.jpg',
+                        ),
+                      ),
+                  )
+              )
+            ],
+          ),
+        )
+    );
+  }
+}
+
+class RecentChat extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 25),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(35),
+          topRight: Radius.circular(35),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            blurRadius: 10,
+            spreadRadius: 2,
+            offset: Offset(0, 2),
+          )
+        ]),
+      child: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            for (int i=0; i < 10; i++ )
+          Padding(padding: EdgeInsets.symmetric(vertical: 15),
+            child: InkWell(
+              onTap: (){},
+              child: Container(
+                height: 65,
+                child: Row(children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(35),
+                    child: Image.asset(
+                      'lib/Images/profile.jpg',
+                      height: 65,
+                      width: 65,),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Doraemon, Nobita, Giant",
+                          style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold),
+                        ),
+
+                        SizedBox(height: 4,),
+
+                        Text("Doraemon の友達の承認待ちです。",
+                        style: TextStyle(
+                          fontSize: 12,
+                        ),)
+                      ],
+                    ),
+                  ),
+                  Spacer(),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 10),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 10),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(
+                                "12:10",
+                                style: TextStyle(fontSize: 15),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Container(
+                                height: 23,
+                                width: 23,
+                                alignment: Alignment.center,
+                                decoration: BoxDecoration(
+                                  color: Color(0xFF113953),
+                                  borderRadius: BorderRadius.circular(25),
+
+                                ),
+                                child: Text(
+                                  "1",
+                                  style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),),
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+                ),
+              ),
+            ),
+          )
+        ],),
+      ),
+      );
+  }
+}
+
+
+
+
 
 
 
